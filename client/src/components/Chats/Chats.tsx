8d6574b2ec47b94958ac83fc6f22280238724d3e
Peter571 <peterkoech1624@gmail.com>
2022-09-10 02:57:20 +0300

    socket io

--- client/src/components/Chats/Chats.tsx
+++ client/src/components/Chats/Chats.tsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import Chat from './Chat'
-import pic from '../../assets/somepic.png'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Chat from "./Chat";
+import pic from "../../assets/somepic.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Chats() {
   const navigate = useNavigate();
 
   return (
     <div className="flex flex-col gap-4 h-screen">
-      <div className='basis-1/8'>
+      <div className="basis-1/8">
         <h1 className="logo" onClick={() => navigate("/")}>
           Task Manager
         </h1>
       </div>
       <h1 className="font-bold basis-1/8">Chats</h1>
       <div className="basis-3/4 flex flex-col gap-2 overflow-auto">
-        <Chat
-          profileImage={pic}
-          userName={"Koecha"}
-          previewText={"Hey Koecha"}
+        {users.map((user, index) => (
+          <Chat
+          key={index}
+          profileImage={user.img}
+          userName={user.name}
+          previewText={user.text}
         />
+        ))}
       </div>
 
-      <div className=''>
+      <div className="">
         <button className="btn">New user</button>
       </div>
     </div>
   );
 }
+
+export const users = [
+  { name: "koecha", text: "Hey koecha", img: pic },
+  { name: "Peter", text: "Hey koecha", img: pic },
+  { name: "Kip", text: "Hey koecha", img: pic },
+  { name: "Joe", text: "Hey koecha", img: pic },
+  { name: "Doe", text: "Hey koecha", img: pic },
+  { name: "Kevin", text: "Hey koecha", img: pic },
+  { name: "John", text: "Hey koecha", img: pic },
+];
--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 export default function Messages() {
+  
   return (
     <div className="basis-1/2 h-screen relative">
-      <h1>Messages</h1>
-      <div className="bg-gray-200 h-[80%] rounded-sm"></div>
+      <h1>Conversations</h1>
+      <div id="messages-container" className="bg-gray-200 h-[80%] rounded-sm">
+
+      </div>
       <div className="absolute w-full bottom-0 flex flex-row gap-2 justify-between">
         <textarea
           className="form__input rounded basis-3/4"
           name="message"
+          placeholder="Type message..."
           id="msg"
           rows={3}
         ></textarea>
