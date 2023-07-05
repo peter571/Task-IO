9a6877e2800ee81c03e7128d72f7e72ac8b37f49
Peter <peterkoech1624@gmail.com>
2023-07-05 03:24:43 +0300

    docker setup

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { ISocket } from "../types";
 
-const URL = import.meta.env.MODE === 'production' ? "https://task-io-socket.onrender.com" : "http://localhost:7000";
+const URL = import.meta.env.MODE === 'production' ? "https://task-io-socket-server-2pqqdimvsq-uc.a.run.app" : "http://localhost:7000";
 
 const socket: ISocket = io(URL, {
   autoConnect: false,
