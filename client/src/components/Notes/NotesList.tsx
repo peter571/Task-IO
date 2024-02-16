43d65c591d7dbfc71bc3ccd0caf8786a7a9be414
Peter <peterkoech1624@gmail.com>
2024-02-16 14:41:27 +0300

    fix delete, update messages bugs

--- client/src/components/Notes/NotesList.tsx
+++ client/src/components/Notes/NotesList.tsx
@@ -9,18 +9,21 @@ import Loader from "components/Loader/Loader";
 export default function NotesList() {
   const { spaceId } = useWorkSpaceContext();
   const { user } = useAccountContext();
-  const {
-    data: notes = [],
-    isLoading,
-  } = useGetUserNotesQuery({
+  const { data: notes, isLoading } = useGetUserNotesQuery({
     workspace_id: spaceId,
     userId: user.userId,
   });
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className="my-2">
-      {isLoading && <Loader />}
-      {!isLoading &&
+      {notes && notes.length === 0 && (
+        <p className="my-5 text-sm">No notes found</p>
+      )}
+      {notes &&
         notes.length > 0 &&
         notes.map((note: NoteProp, idx: number) => (
           <Note key={idx} {...note} />
--- client/src/components/TasksTab/UserTasks.tsx
+++ client/src/components/TasksTab/UserTasks.tsx
@@ -4,21 +4,29 @@ import { useAccountContext } from "context/AccountContext";
 import { TaskProp } from "@/types";
 import Task from "components/TasksTab/Task";
 import { useWorkSpaceContext } from "components/WorkSpace/WorkSpace";
+import Loader from "components/Loader/Loader";
 
 export default function UserTasks() {
   const { user } = useAccountContext();
   const { spaceId } = useWorkSpaceContext();
 
-  const {
-    data: tasks = [],
-    isSuccess,
-  } = useGetUserTasksQuery({ workspace_id: spaceId, userId: user.userId });
+  const { data: tasks = [], isLoading } = useGetUserTasksQuery({
+    workspace_id: spaceId,
+    userId: user.userId
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className="py-2 h-[600px] overflow-y-auto">
-      {isSuccess &&
-        tasks.map((task: TaskProp, idx: number) => (
-          <Task key={idx} {...task} />
-        ))}
+      {tasks && tasks.length === 0 ? (
+        <p className="text-sm">No Tasks</p>
+      ) : (
+        tasks &&
+        tasks.map((task: TaskProp, idx: number) => <Task key={idx} {...task} />)
+      )}
     </div>
   );
 }
